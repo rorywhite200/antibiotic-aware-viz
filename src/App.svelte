c903cbe4e964f8e8dec9abf73f40e701d521bab4
rorywhite200 <125914446+rorywhite200@users.noreply.github.com>
2024-01-08 23:35:55 -0800

    add credits

--- src/App.svelte
+++ src/App.svelte
@@ -5,15 +5,29 @@
 
   $: console.log($width, $height)
 
+  let text = "Web Annex C. WHO AWaRe (access, watch, reserve) classification of antibiotics for evaluation and monitoring of use, 2023. WHO; 2023."
+
 </script>
 
 <main
   bind:clientWidth={$width}
   bind:clientHeight={$height}
 >
-<div style="height: 95%; aspect-ratio:1;">
+<div style="height: 97%; aspect-ratio:1;">
   <Sunburst />
 </div>
+
+
+<div class="citations">
+  {text}
+</div>
+
+<div class="title">
+WHO AWaRe <br>Classification of Antibiotics
+<br>
+<p class="credit">Viz by Rory White</p>
+</div>
+
 
 </main>
 <style>
@@ -22,7 +36,35 @@
     height: 100vh;
     display: flex;
     justify-content: center;
-    align-items: center;
+    align-items: flex-start;
+  }
+
+  .credit {
+    font-size: 0.9rem;
+    font-family: "lorimer-no-2", sans-serif;
+    color: rgb(41, 38, 38);
+  }
+  .title {
+    font-family: "lorimer-no-2", sans-serif;
+    position: absolute;
+    top: 1rem;
+    left: 1rem;
+    font-size: 1.5rem;
+    padding: 0.5rem;
+    color: grey;
+    z-index: -1;
+  }
+
+  .citations {
+    font-family: "lorimer-no-2", sans-serif;
+    position: absolute;
+    bottom: 1rem;
+    right: 1rem;
+    width: 150px;
+    font-size: 0.6rem;
+    padding: 0.5rem;
+    color: grey;
+    z-index: -1;
   }
 
 </style>
